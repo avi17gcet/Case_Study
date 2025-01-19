9b54bb391b6b9a6d81a6002f195340a7d31997e9
avi17gcet <93109175+avi17gcet@users.noreply.github.com>
2025-01-19 23:13:31 +0530

    Delete Snowflake/create_customer_table.sql

--- Snowflake/create_customer_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE customer (
-  id number not null, 
-  first_name varchar, 
-  last_name varchar
-);
-GRANT OWNERSHIP ON TABLE customer TO ROLE {{owner}};
-EXECUTE IMMEDIATE FROM 'insert_customers.sql';
